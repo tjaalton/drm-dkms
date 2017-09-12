3f6b9b63df5a56798c1f74fe61cd013ecd472dba
Timo Aaltonen <tjaalton@debian.org>
2017-09-12 13:48:55 +0300

    update backport.h

--- include/backport.h
+++ include/backport.h
@@ -2,3 +2,7 @@
 #define PCI_VENDOR_ID_REDHAT_QUMRANET    0x1af4
 #define PCI_SUBVENDOR_ID_REDHAT_QUMRANET 0x1af4
 #define PCI_SUBDEVICE_ID_QEMU            0x1100
+
+
+#define __malloc               __attribute__((__malloc__))
+
