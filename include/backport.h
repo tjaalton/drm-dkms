eb6dc7d8be6bce45293bde1be80a46ae75dd5c57
Timo Aaltonen <tjaalton@debian.org>
2019-01-30 13:20:50 +0200

    use bpo prefix for reservation

--- include/backport.h
+++ include/backport.h
@@ -1,8 +1,14 @@
 
-#define PCI_VENDOR_ID_REDHAT_QUMRANET    0x1af4
-#define PCI_SUBVENDOR_ID_REDHAT_QUMRANET 0x1af4
-#define PCI_SUBDEVICE_ID_QEMU            0x1100
+static inline int
+reservation_object_lock_interruptible(struct reservation_object *obj,
+                                      struct ww_acquire_ctx *ctx)
+{
+        return ww_mutex_lock_interruptible(&obj->lock, ctx);
+}
 
+int bpo_reservation_object_get_fences_rcu(struct reservation_object *obj,
+                                      struct dma_fence **pfence_excl,
+                                      unsigned *pshared_count,
+                                      struct dma_fence ***pshared);
 
-#define __malloc               __attribute__((__malloc__))
 
